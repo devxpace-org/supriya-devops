7942b5fc082a5a40c1ba8e2cffc6bb8e83316246
supriyagoud5 <135716312+supriyagoud5@users.noreply.github.com>
2023-06-27 16:53:23 -0400

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/helloworld', (req, res) => {
-  res.send('you have done a great job!')
+app.get('/nicejob', (req, res) => {
+  res.send('you did good job!')
 })
 
 app.listen(port, () => {
