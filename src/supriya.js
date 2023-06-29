54fa5e0eeb8a36d78718b221a53fb2255cddfa76
supriyagoud5 <135716312+supriyagoud5@users.noreply.github.com>
2023-06-29 13:32:23 -0400

    Update supriya.js

--- src/supriya.js
+++ src/supriya.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/supriya', (req, res) => {
-  res.send('Hi, This is supriya!')
+app.get('/chethana', (req, res) => {
+  res.send('Hi, This is chethana!')
 })
 
 app.listen(port, () => {
